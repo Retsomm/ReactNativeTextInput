85bc77c7d183fb3e7c6d7dbd410559d400655e9b
Yu Ting Zhan <83443363+Retsomm@users.noreply.github.com>
2023-09-10 11:04:38 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import {
   Text,
   View,
   TextInput,
-  TouchableOpacity
+  TouchableOpacity,
+  TouchableWithoutFeedback, 
+  Keyboard
 } from "react-native";
 
 export default function App() {
@@ -12,113 +14,115 @@ export default function App() {
   const [passportCode, setPassportCode] = useState("");
   const [button, setButton] = useState("登入");
   return (
-    <View style={styles.container}>
-      <Text
-        style={{
-          color: "white",
-          fontSize: "20px",
-          margin: "10px",
-          width: "300px",
-          borderBottom: "1px solid #fff",
-          paddingBottom: "20px"
-        }}
-      >
-        會員登入
-      </Text>
-      <Text
-        style={{
-          color: "white",
-          fontSize: "16px",
-          margin: "10px",
-          width: "300px"
-        }}
-      >
-        您輸入的手機號碼為{phoneCode}
-      </Text>
-      <TextInput
-        maxLength={10}
-        style={{
-          height: 50,
-          width: 300,
-          borderRadius: 0,
-          borderColor: "darkgray",
-          backgroundColor: "white",
-          color: "black",
-          fontSize: 28,
-          textAlign: "center"
-        }}
-        onChangeText={(text) => setPhoneCode(text)}
-        keyboardType={"numeric"}
-        value={phoneCode}
-        autoFocus={true}
-      />
-      <Text
-        style={{
-          color: "white",
-          fontSize: "16px",
-          margin: "10px",
-          width: "300px"
-        }}
-      >
-        密碼
-      </Text>
-      <TextInput
-        maxLength={4}
-        style={{
-          height: 50,
-          width: 300,
-          borderRadius: 0,
-          borderColor: "darkgray",
-          backgroundColor: "white",
-          color: "black",
-          fontSize: 28,
-          textAlign: "center"
-        }}
-        onChangeText={(text) => setPassportCode(text)}
-        keyboardType={"numeric"}
-        value={passportCode}
-        secureTextEntry={true}
-      />
-      <TouchableOpacity
-        style={{
-          backgroundColor: "#00aeef",
-          borderRadius: 5,
-          width: 300,
-          height: 40,
-          justifyContent: "center",
-          margin: 40
-        }}
-        onPress={() => {
-          if (phoneCode === "0912345678" && passportCode === "0000") {
-            setButton("登入成功");
-          } else {
-            setButton("登入");
-          }
-        }}
-      >
-        <Text style={{ color: "white", textAlign: "center", fontSize: 20 }}>
-          {button}
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <View style={styles.container}>
+        <Text
+          style={{
+            color: "white",
+            fontSize: "20px",
+            margin: "10px",
+            width: "300px",
+            borderBottom: "1px solid #fff",
+            paddingBottom: "20px"
+          }}
+        >
+          會員登入
         </Text>
-      </TouchableOpacity>
-      {phoneCode.length === 10 ? (
-        phoneCode === "0912345678" ? (
-          <Text style={{ color: "yellow" }}>手機號碼輸入成功</Text>
+        <Text
+          style={{
+            color: "white",
+            fontSize: "16px",
+            margin: "10px",
+            width: "300px"
+          }}
+        >
+          您輸入的手機號碼為{phoneCode}
+        </Text>
+        <TextInput
+          maxLength={10}
+          style={{
+            height: 50,
+            width: 300,
+            borderRadius: 0,
+            borderColor: "darkgray",
+            backgroundColor: "white",
+            color: "black",
+            fontSize: 28,
+            textAlign: "center"
+          }}
+          onChangeText={(text) => setPhoneCode(text)}
+          keyboardType={"numeric"}
+          value={phoneCode}
+          autoFocus={true}
+        />
+        <Text
+          style={{
+            color: "white",
+            fontSize: "16px",
+            margin: "10px",
+            width: "300px"
+          }}
+        >
+          密碼
+        </Text>
+        <TextInput
+          maxLength={4}
+          style={{
+            height: 50,
+            width: 300,
+            borderRadius: 0,
+            borderColor: "darkgray",
+            backgroundColor: "white",
+            color: "black",
+            fontSize: 28,
+            textAlign: "center"
+          }}
+          onChangeText={(text) => setPassportCode(text)}
+          keyboardType={"numeric"}
+          value={passportCode}
+          secureTextEntry={true}
+        />
+        <TouchableOpacity
+          style={{
+            backgroundColor: "#00aeef",
+            borderRadius: 5,
+            width: 300,
+            height: 40,
+            justifyContent: "center",
+            margin: 40
+          }}
+          onPress={() => {
+            if (phoneCode === "0912345678" && passportCode === "0000") {
+              setButton("登入成功");
+            } else {
+              setButton("登入");
+            }
+          }}
+        >
+          <Text style={{ color: "white", textAlign: "center", fontSize: 20 }}>
+            {button}
+          </Text>
+        </TouchableOpacity>
+        {phoneCode.length === 10 ? (
+          phoneCode === "0912345678" ? (
+            <Text style={{ color: "yellow" }}>手機號碼輸入成功</Text>
+          ) : (
+            <Text style={{ color: "red" }}>手機號碼輸入錯誤</Text>
+          )
         ) : (
-          <Text style={{ color: "red" }}>手機號碼輸入錯誤</Text>
-        )
-      ) : (
-        <Text style={{ color: "red" }}>請輸入手機號碼</Text>
-      )}
-      {passportCode.length === 4 ? (
-        passportCode === "0000" ? (
-          <Text style={{ color: "yellow" }}>密碼輸入成功</Text>
+          <Text style={{ color: "red" }}>請輸入手機號碼</Text>
+        )}
+        {passportCode.length === 4 ? (
+          passportCode === "0000" ? (
+            <Text style={{ color: "yellow" }}>密碼輸入成功</Text>
+          ) : (
+            <Text style={{ color: "red" }}>密碼輸入錯誤</Text>
+          )
         ) : (
-          <Text style={{ color: "red" }}>密碼輸入錯誤</Text>
-        )
-      ) : (
-        <Text style={{ color: "red" }}>請輸入密碼</Text>
-      )}
-    </View>
+          <Text style={{ color: "red" }}>請輸入密碼</Text>
+        )}
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
